3e9b4117d36fe45b13b68c002d90ccc37b4e61eb
RobertGabdullin <71557273+RobertGabdullin@users.noreply.github.com>
2021-08-26 16:56:20 +0300

    Update ex4.c

--- week2/ex4.c
+++ week2/ex4.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 void Swap(int* a, int* b) {
 	int t = *a;
@@ -7,12 +9,17 @@ void Swap(int* a, int* b) {
 }
 
 int main(void) {
-	
+
 	int a, b;
-	scanf_s("%d%d", &a, &b);
+	char s1[11], s2[11];
+
+	printf("Enter the first number: "); fgets(s1, 11, stdin);
+	printf("Enter the second number: "); fgets(s2, 11, stdin);
+
+	a = atoi(s1), b = atoi(s2);
 
 	Swap(&a, &b);
-	
-	printf("%d %d", a, b);
+
+	printf("Now the first number equals to %d and the second equals to %d", a, b);
 
 }
