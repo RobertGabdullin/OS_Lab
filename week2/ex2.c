fac578ad4385601debdcf0125445dd99578bd29d
RobertGabdullin <71557273+RobertGabdullin@users.noreply.github.com>
2021-08-26 17:08:13 +0300

    Update ex2.c

--- week2/ex2.c
+++ week2/ex2.c
@@ -3,11 +3,13 @@
 #define MAXN 10000
 
 int main(void) {
-	
-	char s[MAXN]; s[0] = 0;
-	gets(s);
-	
-	for (int i = strlen(s) - 1; i >= 0; i--)
+
+	char s[MAXN];
+	printf("Enter your string: "); fgets(s, MAXN, stdin);
+
+	printf("Reversed version: ");
+
+	for (int i = strlen(s) - 2; i >= 0; i--)
 		printf("%c", s[i]);
-	
+
 }
